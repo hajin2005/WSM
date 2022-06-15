42b9bc3462a1465448d4daa6b876e157778adf9d
hajin <w2128@e-mirim.hs.kr>
2022-06-15 12:02:46 +0900

    js change toggleIcon

--- 0608_school cafeteria/js/main.js
+++ 0608_school cafeteria/js/main.js
@@ -1,14 +1,18 @@
 //show/hide menu = toggle menu
 const toggleMenu = (toggleId, navListId) => {
-    //html ==> js
+    //html ==> js 요소 가져오기
     const toggle = document.getElementById(toggleId);
     const navList = document.getElementById(navListId);
+    const toggleIcon = toggle.getElementsByTagName("i")[0];
 
     if (toggle && navList) {
         //toggle click 됐을 때
         toggle.addEventListener('click', () => {
             //show/hide menu, show-menu
             navList.classList.toggle('show-menu');
+            //change toggle item: bx menu<->bx-x-circle
+            toggleIcon.classList.toggle("bx-menu");
+            toggleIcon.classList.toggle("bx-x-circle")
         })
 
     }
